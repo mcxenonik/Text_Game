e8650b4ac2055029750f4370710925a7c6c5815d
mcxenonik <01112955@pw.edu.pl>
2021-03-23 19:32:33 +0100

    Implemented info() method

--- classes.py
+++ classes.py
@@ -3,13 +3,25 @@ class Player():
     Class Player. Contains attributes:
     :param name: player's name
     :tupe name: str
+
+    :param lives: player's lives left
+    :type lives: int
     """
     def __init__(self, name, lives=1):
         self.name = name
         self.lives = lives
 
     def info(self):
-        if (self.lives == 1):
-            return f'My name is {self.name}. I have 1 life left'
-        else:
-            return f'My name is {self.name}. I have {self.lives} lives left'
+        """
+        Returns basic information about player.
+        """
+        # if (self.lives == 1):
+        #     return f'My name is {self.name}. I have 1 life left'
+        # else:
+        #     return f'My name is {self.name}. I have {self.lives} lives left'
+        lives = 'life' if (self.lives == 1) else 'lives'
+
+        return f'My name is {self.name}. I have {self.lives} {lives} left'
+
+    def __str__(self):
+        return self.info()
--- test_classes.py
+++ test_classes.py
@@ -17,9 +17,22 @@ def test_creat_player_with_lives():
 
 def test_introduce():
     player = Player('Jurek Ogorek', 3)
+
     assert player.info() == 'My name is Jurek Ogorek. I have 3 lives left'
+
     player = Player('Jurek Ogorek', 1)
+
     assert player.info() == 'My name is Jurek Ogorek. I have 1 life left'
+
+
+def test_introduce_as_str():
+    player = Player('Jurek Ogorek', 3)
+
+    assert str(player) == player.info()
+
+    player = Player('Jurek Ogorek', 1)
+
+    assert str(player) == player.info()
 
 
 def test_compare_players():
