a21eb91e4219140567e68c7ebd1f9cc9286a87cc
mcxenonik <01112955@pw.edu.pl>
2021-03-24 02:40:16 +0100

    Added Enemy() class and unit tests

--- classes.py
+++ classes.py
@@ -8,11 +8,13 @@ class Player():
     :type lives: int
     """
     def __init__(self, name, lives=1):
-        self._name = name
+        if (not name):
+            raise ValueError('Name cannot be empty')
         if (int(lives) < 0):
             raise ValueError('Lives cannot be negative')
-        else:
-            self._lives = int(lives)
+
+        self._lives = int(lives)
+        self._name = str(name)
 
     def get_name(self):
         return self._name
@@ -20,29 +22,82 @@ class Player():
     def set_name(self, new_name):
         if (not new_name):
             raise ValueError('Name cannot be empty')
-        else:
-            self._name = str(new_name).title()
+
+        self._name = str(new_name).title()
 
     def get_lives(self):
         return self._lives
 
     def set_lives(self, new_lives):
-        if (new_lives < 0):
-            raise ValueError('Lives cannot be negative')
-        else:
-            self._lives = int(new_lives)
+        self._lives = max(0, int(new_lives))
 
     def info(self):
         """
         Returns basic information about player.
         """
-        # if (self.lives == 1):
-        #     return f'My name is {self.name}. I have 1 life left'
-        # else:
-        #     return f'My name is {self.name}. I have {self.lives} lives left'
         lives = 'life' if (self._lives == 1) else 'lives'
 
         return f'My name is {self._name}. I have {self._lives} {lives} left'
+
+    def __str__(self):
+        return self.info()
+
+
+class Enemy():
+    """
+    Class Enemy. Contains attributes:
+    :param name: enemy's name
+    :tupe name: str
+
+    :param health: enemy's health left
+    :type health: int
+    """
+    def __init__(self, name, health=1):
+        if (not name):
+            raise ValueError('Name cannot be empty')
+        if (int(health) < 0):
+            raise ValueError('Health cannot be negative')
+
+        self._name = str(name)
+        self._health = int(health)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, new_name):
+        if (not new_name):
+            raise ValueError('Name cannot be empty')
+
+        self._name = str(new_name).title()
+
+    def get_health(self):
+        return self._health
+
+    def set_health(self, new_health):
+        self._health = max(0, int(new_health))
+
+    def take_damage(self, damage):
+        """
+        Reduces health of enemy by damage.
+        """
+        if (int(damage) <= 0):
+            raise ValueError('Damage cannot be zero or negative')
+
+        self._health -= min(damage, self._health)
+
+    def is_alive(self):
+        """
+        Checks if enemy is alive or not.
+        """
+        return self._health > 0
+
+    def info(self):
+        """
+        Returns basic information about enemy.
+        """
+        points = 'point' if (self._health == 1) else 'points'
+
+        return f'My name is {self._name}. I have {self._health} health {points} left'
 
     def __str__(self):
         return self.info()
--- test_classes.py
+++ test_classes.py
@@ -1,4 +1,4 @@
-from classes import Player
+from classes import Player, Enemy
 import pytest
 
 
@@ -18,11 +18,7 @@ def test_creat_player_with_lives():
 
 def test_creat_player_with_negative_lives():
     with pytest.raises(ValueError):
-        player = Player('Jurek Ogorek', -1)
-
-    player = Player('Jurek Ogorek')
-
-    assert player.get_lives() == 1
+        Player('Jurek Ogorek', -1)
 
 
 def test_introduce():
@@ -97,5 +93,142 @@ def test_set_lives_negative():
 
     assert player.get_lives() == 1
 
+    player.set_lives(-1)
+
+    assert player.get_lives() == 0
+
+
+def test_enemy_creat():
+    enemy = Enemy('Piekielny Demon')
+
+    assert enemy.get_name() == 'Piekielny Demon'
+    assert enemy.get_health() == 1
+
+
+def test_enemy_creat_with_health():
+    enemy = Enemy('Piekielny Demon', 4)
+
+    assert enemy.get_name() == 'Piekielny Demon'
+    assert enemy.get_health() == 4
+
+
+def test_enemy_creat_with_negative_health():
     with pytest.raises(ValueError):
-        player.set_lives(-1)
+        Enemy('Piekielny Demon', -1)
+
+
+def test_enemy_introduce():
+    enemy = Enemy('Piekielny Demon', 3)
+
+    assert enemy.info() == 'My name is Piekielny Demon. I have 3 health points left'
+
+    enemy = Enemy('Piekielny Demon', 1)
+
+    assert enemy.info() == 'My name is Piekielny Demon. I have 1 health point left'
+
+
+def test_enemy_introduce_as_str():
+    enemy = Enemy('Piekielny Demon', 3)
+
+    assert str(enemy) == enemy.info()
+
+    enemy = Enemy('Piekielny Demon', 1)
+
+    assert str(enemy) == enemy.info()
+
+
+def test_enemy_set_name():
+    enemy = Enemy('Piekielny Demon')
+
+    assert enemy.get_name() == 'Piekielny Demon'
+
+    enemy.set_name('Lodowy Olbrzym')
+
+    assert enemy.get_name() == 'Lodowy Olbrzym'
+
+
+def test_enemy_set_name_empty():
+    enemy = Enemy('Piekielny Demon')
+
+    with pytest.raises(ValueError):
+        enemy.set_name('')
+
+
+def test_enemy_set_name_lowercase():
+    enemy = Enemy('Piekielny Demon')
+
+    assert enemy.get_name() == 'Piekielny Demon'
+
+    enemy.set_name('LoDOwy OlBrZym')
+
+    assert enemy.get_name() == 'Lodowy Olbrzym'
+
+
+def test_enemy_set_health():
+    enemy = Enemy('Piekielny Demon')
+
+    assert enemy.get_health() == 1
+
+    enemy.set_health(2)
+
+    assert enemy.get_health() == 2
+
+
+def test_enemy_set_health_zero():
+    enemy = Enemy('Piekielny Demon')
+
+    assert enemy.get_health() == 1
+
+    enemy.set_health(0)
+
+    assert enemy.get_health() == 0
+
+
+def test_enemy_set_health_negative():
+    enemy = Enemy('Piekielny Demon')
+
+    assert enemy.get_health() == 1
+
+    enemy.set_health(-1)
+
+    assert enemy.get_health() == 0
+
+
+def test_enemy_take_damage():
+    enemy = Enemy('Piekielny Demon', 10)
+
+    assert enemy.get_health() == 10
+
+    enemy.take_damage(5)
+
+    assert enemy.get_health() == 5
+
+
+def test_enemy_take_lethal_damage():
+    enemy = Enemy('Piekielny Demon', 10)
+
+    assert enemy.get_health() == 10
+
+    enemy.take_damage(12)
+
+    assert enemy.get_health() == 0
+
+
+def test_enemy_take_negative_damage():
+    enemy = Enemy('Piekielny Demon', 10)
+
+    with pytest.raises(ValueError):
+        enemy.take_damage(-3)
+
+
+def test_enemy_is_alive():
+    enemy = Enemy('Piekielny Demon', 10)
+
+    assert enemy.is_alive()
+
+
+def test_enemy_is_not_alive():
+    enemy = Enemy('Piekielny Demon', 10)
+    enemy.take_damage(10)
+
+    assert not (enemy.is_alive())
